766bf2ae6eac75d3c120aa5bb4a73cb5c72111cf
Andrew Raim <andrew.raim@gmail.com>
2023-01-26 10:33:01 -0500

    Updates to Roxygen

--- R/mlogit.R
+++ R/mlogit.R
@@ -1,11 +1,36 @@
-# Transform from probability simplex S^J to R^(J-1)
+#' Multinomial logit transformation
+#' 
+#' The multinomial logit transformation and its inverse.
+#'
+#' @param x A point in Euclidean space \eqn{R^{J-1}}.
+#' @param p A point in the probability simplex \eqn{S^J}.
+#' @param ref Index of the reference category. Default is \code{1}, the first
+#' category.
+#'
+#' @details 
+#' \itemize{
+#' \item{mlogit}{Transforms from probability simplex \eqn{S^J} to \eqn{R^{J-1}}.}
+#' \item{inv_mlogit}{Transforms from \eqn{R^{J-1}} to probability simplex \eqn{S^J}.}
+#' }
+#' 
+#' @name mlogit
+#' @examples
+#' p = 1:5 / sum(1:5)
+#' x = mlogit(p)
+#' p_new = inv_mlogit(x)
+#' 
+NULL
+
+#' @name mlogit
+#' @export
 mlogit = function(p, ref = 1)
 {
 	log(p[-ref] / p[ref])
 }
 
-# Transform from R^(J-1) to probability simplex S^J
-inv.mlogit = function(x, ref = 1)
+#' @name mlogit
+#' @export
+inv_mlogit = function(x, ref = 1)
 {
 	p = numeric(length(x) + 1)
 	z = exp(x)
--- R/rqres.R
+++ R/rqres.R
@@ -1,10 +1,33 @@
 #  Set eps to zero to avoid using random jitter
-rqres <- function(y, F, eps = 1e-6)
+#' Randomized Quantile Residuals
+#' 
+#' Compute randomized quantile residuals (Dunn & Smyth, 1996).
+#'
+#' @param y Vector of univariate observations
+#' @param p_dist A cumulative distribution function. Typically the CDF of a
+#' fitted model with with parameters evaluated at  estimates.
+#' @param eps A small positive number needed for discrete distributions
+#'
+#' @return Vector of residuals
+#' 
+#' @references
+#' Peter K. Dunn & Gordon K. Smyth (1996) Randomized Quantile Residuals,
+#' Journal of Computational and Graphical Statistics, 5:3, 236-244,
+#' DOI: 10.1080/10618600.1996.10474708.
+#' 
+#' @examples
+#' mu_true = 10
+#' y = rpois(200, mu_true)
+#' mu_hat = mean(x)
+#' p_dist = function(x) { ppois(y, mu_hat) }
+#' res = rqres(y, p_dist)
+#'
+#' @export
+rqres = function(y, p_dist, eps = 1e-6)
 {
-	n <- length(y)
-	FL <- F(y - eps)
-	FU <- F(y)
-	u <- runif(n, min = FL, max = FU)
-	qres <- qnorm(u)
-	return(qres)
+	n = length(y)
+	FL = p_dist(y - eps)
+	FU = p_dist(y)
+	u = runif(n, min = FL, max = FU)
+	qnorm(u)
 }
